db00931838d197a2d5027dc132443078f9fff3be
SUNNY BHAVEEN CHANDRA <34603575+c17hawke@users.noreply.github.com>
2023-04-05 08:45:48 +0530

    updated code

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import os
 os.makedirs("src", exist_ok=True)
 
 with open("src/example.py", "w+") as f:
-    f.write("""print("I am the change")\n""")
+    f.write("""print("I am the change 2")\n""")
 
 print("Complete!!")
